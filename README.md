9ccbcbee44c06ef40377adb6133dab5fcc242364
arex0 <33726215+arex0@users.noreply.github.com>
2020-04-15 19:17:17 +0800

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # web-mobile-drag
+
+A polyfill of drag in mobile.
+
+### base
+- Touch API
+- DragEvent() constructor
+### features
+- Uncompressed 6 KB only
+- No third-party dependencies
+- Multi-drag
---
+++ web-mobile-drag.js
@@ -0,0 +1,134 @@
+
+const ArrayMethods = Array.prototype
+const hypot = (p1, p2) => Math.hypot(p1.pageX - p2.pageX, p1.pageY - p2.pageY)
+const copyTouch = touch => ({ id: touch.identifier, pageX: touch.pageX, pageY: touch.pageY })
+const findTouch = (touches, id) => ArrayMethods.find.call(touches, ({ identifier }) => identifier == id)
+function on(ele,type,handle,opts){ele.addEventListener(type,handle,opts)}
+function un(ele,type,handle,opts){ele.removeEventListener(type,handle,opts)}
+function draggable(ele){
+    let touch = null
+    on(ele,'contextmenu',e=>e.preventDefault())
+    on(ele,'touchstart',e=>{
+        if (touch) return
+        let cancel = false,
+            u = e.changedTouches.length-1,
+            tc = e.changedTouches[u]
+        touch = copyTouch(tc)
+        function cancelDrag(e){
+            if(e.type!='touchstart'&&touch&&e.type=='touchmove'&&ArrayMethods.every.call(e.changedTouches,tc=>hypot(tc,touch)>50)) return
+            cancel = true
+            touch = null
+        }
+        on(document,'touchstart',()=>on(document,'touchstart',cancelDrag,{once:true}),{once:true})
+        on(document,'touchmove',cancelDrag,{passive:false})
+        on(document,'touchend',cancelDrag,{once:true})
+        on(document,'touchcancel',cancelDrag,{once:true})
+        setTimeout(()=>{
+            un(document,'touchcancel',cancelDrag,{once:true})
+            un(document,'touchend',cancelDrag,{once:true})
+            un(document,'touchmove',cancelDrag,{passive:false})
+            un(document,'touchstart',cancelDrag,{once:true})
+            if(!cancel){
+                let shadow = ele.cloneNode(true), style = shadow.style
+                    src = ele.getBoundingClientRect(), srcX = src.x, srcY = src.y,
+                    baseX = tc.pageX, baseY = tc.pageY
+                Object.entries(getComputedStyle(ele)).forEach(([attr,val])=>{style[attr] = val})
+                style.position = 'fixed'
+                style.left = srcX+'px'
+                style.top = srcY+'px'
+                style.opacity = parseFloat(document.body.style.opacity||1) * 0.5
+                style.pointerEvents = 'none'
+                style.zIndex = 999
+                document.body.appendChild(shadow)
+
+                let target = null, overTimer = null, fireover = true
+                function getElementByPoint(e){
+                    let el = document.elementFromPoint(e.clientX, e.clientY)
+                    while (el && getComputedStyle(el).pointerEvents == 'none') {
+                        el = el.parentElement;
+                    }
+                    return el;
+                }
+                function dispatchEvent(e){
+                    shadow.remove()
+                    let t = getElementByPoint(e)
+                    document.body.appendChild(shadow)
+                    let init = {
+                        screenX: e.screenX,
+                        screenY: e.screenY,
+                        clientX: e.clientX,
+                        clientY: e.clientY,
+                        pageX: e.pageX,
+                        pageY: e.pageY,
+                    }
+                    if(t!=target){
+                        init.relatedTarget = target
+                        t.dispatchEvent(new DragEvent('dragenter',init))
+                        init.relatedTarget = t
+                        target.dispatchEvent(new DragEvent('dragleave',init))
+                        target = t
+                        init.relatedTarget = null
+                    }
+                    clearInterval(overTimer)
+                    target.dispatchEvent(new DragEvent('dragover',init))
+                    overTimer = setInterval(()=>target.dispatchEvent(new DragEvent('dragover',init)),350)
+                }
+                function RenderDragMove(e){
+                    ele.dispatchEvent(new DragEvent('drag',e))
+                    shadow.style.left = srcX-baseX+e.pageX+'px'
+                    shadow.style.top  = srcY-baseY+e.pageY+'px'
+                }
+                function DragMove(e){
+                    let t = findTouch(e.changedTouches,touch.id)
+                    if (t){
+                        e.preventDefault()
+                        e.stopPropagation()
+                        requestAnimationFrame(()=>{
+                            dispatchEvent(t)
+                            RenderDragMove(t)
+                        })
+                    }
+                }
+                function DragEnd(e){
+                    let t = findTouch(e.changedTouches,touch.id)
+                    if(t&&(t.identifier==touch.id)){
+                        touch = null
+                        shadow.remove()
+                        shadow = null
+                        clearInterval(overTimer)
+                        target.dispatchEvent(new DragEvent('drop',{
+                            screenX: t.screenX,
+                            screenY: t.screenY,
+                            clientX: t.clientX,
+                            clientY: t.clientY,
+                            pageX: t.pageX,
+                            pageY: t.pageY,
+                        }))
+                        ele.dispatchEvent(new DragEvent('dragend',t))
+                        un(document,'touchcancel',DragEnd)
+                        un(document,'touchend',DragEnd)
+                        un(document,'touchmove',DragMove,{passive:false})
+                    }
+                }
+                ele.dispatchEvent(new DragEvent('dragstart', tc))
+                ele.dispatchEvent(new DragEvent('dragenter',tc))
+                target = ele
+                let evt = new DragEvent('dragover',{
+                    screenX: e.screenX,
+                    screenY: e.screenY,
+                    clientX: e.clientX,
+                    clientY: e.clientY,
+                    pageX: e.pageX,
+                    pageY: e.pageY,
+                })
+                ele.dispatchEvent(evt)
+                overTimer = setInterval(()=>ele.dispatchEvent(evt),350)
+                on(document,'touchmove',DragMove,{passive:false})
+                on(document,'touchend',DragEnd)
+                on(document,'touchcancel',DragEnd)
+
+            }
+        },300)
+    })
+}
+document.querySelectorAll('[draggable="true"]').forEach(draggable)
